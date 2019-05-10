3326a9aea4f3b9e5a9cd2769efab81bd51d96708
Oleksandr Kostryba <kostryban1@gmail.com>
2019-05-10 11:47:37 +0300

    added server

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+var port = process.env.PORT || 3000;
+
+app.get("/", function (request, response) {
+    response.send({name: 'Alex'});
+});
+
+app.get("/users", function (request, response) {
+    const users = [
+        {
+            name: 'test1'
+        },
+        {
+            name: 'test2'
+        },
+    ];
+    response.send(users);
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+});
